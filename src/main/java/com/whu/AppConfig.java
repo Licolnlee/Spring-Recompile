31cad7ef1136073978b1ba3db2ca64e150dcbbd0
14798159638 <me@licolnlee.icu>
2023-11-14 11:57:58 +0800

    202311141157

---
+++ src/main/java/com/whu/AppConfig.java
@@ -0,0 +1,7 @@
+package com.whu;
+
+import com.spring.ComponentScan;
+
+@ComponentScan("com.whu.service")
+public class AppConfig {
+}
---
+++ src/main/java/com/whu/Test.java
@@ -0,0 +1,9 @@
+package com.whu;
+
+import com.spring.WhuApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        WhuApplicationContext applicationContext = new WhuApplicationContext(AppConfig.class);
+    }
+}
