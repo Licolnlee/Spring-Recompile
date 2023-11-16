2763223334023dd46b611060899555f2bcc78494
14798159638 <me@licolnlee.icu>
2023-11-16 20:33:03 +0800

    202311162032

--- src/main/java/com/whu/Test.java
+++ src/main/java/com/whu/Test.java
@@ -5,5 +5,8 @@ import com.spring.WhuApplicationContext;
 public class Test {
     public static void main(String[] args) {
         WhuApplicationContext applicationContext = new WhuApplicationContext(AppConfig.class);
+
+        Object userService = applicationContext.getBean("userService");//如果是单例bean，则每次都是同一个对象
+        Object userService1 = applicationContext.getBean("userService");//如果是prototype原型bean，则每次都是新的对象
     }
 }
